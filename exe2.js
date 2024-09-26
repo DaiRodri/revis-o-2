bf2a2c624106ceddd64601fb83e6d056af3bb5c6
DaiRodri <daianarodri@gmail.com>
2024-09-26 16:55:11 -0300

    Add files via upload

---
+++ exe2.js
@@ -0,0 +1,26 @@
+//Peça ao usuário para inserir sua idade. Use operadores relacionais como <, >, ==, != para determinar se o usuário é 
+//maior de idade, menor de idade, ou se tem a idade exata de 18 anos. Este exercício ajuda a consolidar o uso de 
+//operadores relacionais para comparar valores.-->
+
+let idade = 19
+
+if (idade < 18){
+console.log( " menor de idade")
+
+}
+
+if (idade > 18){
+    console.log(" maior de idade")
+
+}
+
+
+if (idade == 18){
+
+    console.log (" idade exata de 18 anos")   
+}
+
+
+
+
+
